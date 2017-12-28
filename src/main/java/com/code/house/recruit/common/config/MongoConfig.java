db49cc65b9ecd730a3aceed2c691b3bf69f05383
mm's <Marek00Malik@gmail.com>
2017-12-28 21:21:16 +0100

    spring boot next version

--- src/main/java/com/code/house/recruit/common/config/MongoConfig.java
+++ src/main/java/com/code/house/recruit/common/config/MongoConfig.java
@@ -2,7 +2,10 @@ package com.code.house.recruit.common.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -10,18 +13,22 @@ import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
+@RequiredArgsConstructor
 @EntityScan(basePackages = {"com.code.house.recruit.common.nosql.doc"})
 @EnableReactiveMongoRepositories(basePackages = {"com.code.house.recruit.common.nosql.repos"})
+@EnableConfigurationProperties(MongoProperties.class)
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
-    @Bean
-    public MongoClient reactiveMongoClient() {
-        return MongoClients.create();
-    }
+    private final MongoProperties mongoProperties;
 
     @Override
     protected String getDatabaseName() {
-        return "test";
+        return mongoProperties.getMongoClientDatabase();
+    }
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create(mongoProperties.determineUri());
     }
 
     @Bean
