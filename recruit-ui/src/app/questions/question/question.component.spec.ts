4d9e8aee41a143254cdb85fbbb3beee72806f9fd
mm's <Marek00Malik@gmail.com>
2018-07-22 10:54:02 +0200

    first attempt to setup true question unit test with karma - failure

--- recruit-ui/src/app/questions/question/question.component.spec.ts
+++ recruit-ui/src/app/questions/question/question.component.spec.ts
@@ -1,6 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
-import { QuestionComponent } from './question.component';
+import { QuestionComponent }            from './question.component';
+import { routes }                       from "../../shared/routes/full-layout.routes";
+import { RouterTestingModule }          from "@angular/router/testing";
+import { ActivatedRoute, Data, Router } from "@angular/router";
+import { Observable }                   from "rxjs/Observable";
+import { QuestionService }              from "../../shared/services/question.service";
+import { QuestionnaireService }         from "../../shared/services/questionnaire.service";
 
 describe('QuestionComponent', () => {
   let component: QuestionComponent;
@@ -8,18 +14,29 @@ describe('QuestionComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ QuestionComponent ]
-    })
-    .compileComponents();
+      declarations: [ QuestionComponent ],
+      imports: [ RouterTestingModule.withRoutes(routes)  ],
+      providers : [
+        QuestionService, QuestionnaireService,
+        {
+          provide :  ActivatedRoute ,
+          useValue: {
+            data : new Observable<Data>(observable =>  observable.next({"isNew" : false ,  "questionType": "Question" }))
+          }
+        }
+      ]
+    });
+
   }));
 
-  beforeEach(() => {
+  it('should create', fakeAsync(() => {
     fixture = TestBed.createComponent(QuestionComponent);
+    TestBed.get(Router).navigate(['/questions/by-id/1']);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    tick();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    expect(component.isNew).toBeFalsy();
+    expect(component.withAnswer).toBeFalsy();
+    // expect(component.questionId).toBe(1);
+  }));
 });
